a03cbaddd2c09010cdf431a49f45dc3007d25b18
Anil Ozsoy <anilozsoy@gmail.com>
2023-07-26 16:39:16 +0200

    🏷️renamed file name

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post } from '@nestjs/common';
-import { AuthService } from './authService';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
