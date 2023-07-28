4887d393f80e35d64850ca9425baa541e204d5c5
Anil Ozsoy <anilozsoy@gmail.com>
2023-07-28 12:39:34 +0200

    💼 added signup logic to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon2 from 'argon2';
+import { ifError } from 'assert';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
-  signup() {
-    return { message: 'User has been created' };
+  async signUp(dto) {
+    try {
+      const hashedPassword = await argon2.hash(dto.password);
+      const user = await this.prismaService.user.create({
+        data: {
+          fullName: dto.fullName,
+          email: dto.email,
+          password: hashedPassword,
+        },
+      });
+      console.log('User has been created');
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          return { message: 'This user already exists in our db' };
+        }
+      }
+    }
   }
-  signin() {
+  signIn() {
     return { message: 'User signed in' };
   }
 }
