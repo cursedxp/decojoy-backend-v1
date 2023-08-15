2e1f9fecbe7494b13c1e77ed78f87c2e2cd76c7e
Anil Ozsoy <anilozsoy@gmail.com>
2023-08-15 19:12:07 +0200

    🔃 added batch image support to the visionService

--- src/vision-service/vision-service.ts
+++ src/vision-service/vision-service.ts
@@ -1,19 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { ImageAnnotatorClient } from '@google-cloud/vision';
+import { ImageAnnotatorClient, protos } from '@google-cloud/vision';
 import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class VisionService {
   private client: ImageAnnotatorClient;
+
   constructor(private config: ConfigService) {
     this.client = new ImageAnnotatorClient({
       keyFilename: this.config.get('KEY_LOCATION'),
     });
   }
-  async detectObjects(imagePath: string) {
-    const [result] = await this.client.objectLocalization(imagePath);
-    const objects = result.localizedObjectAnnotations;
-    console.log(objects);
-    return objects;
+
+  async detectObjects(config: VisionConfig) {
+    const requests: protos.google.cloud.vision.v1.IAnnotateImageRequest[] =
+      config.imageUrls.map((url) => {
+        return {
+          image: {
+            source: {
+              imageUri: url,
+            },
+          },
+          features: [{ type: 'OBJECT_LOCALIZATION' }],
+        };
+      });
+
+    const [response] = await this.client.batchAnnotateImages({ requests });
+
+    return response.responses.map((res) => res.localizedObjectAnnotations);
   }
+}
+
+interface VisionConfig {
+  imageUrls: string[];
 }
