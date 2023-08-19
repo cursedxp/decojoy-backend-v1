9452d036def5b9374ae1e0e8182126fba4852de4
Anil Ozsoy <anilozsoy@gmail.com>
2023-08-19 17:33:46 +0200

    🕹️updated image storage module for image ops service

--- src/image-storage/imageStorage.controller.ts
+++ src/image-storage/imageStorage.controller.ts
@@ -12,6 +12,10 @@ import { ConfigService } from '@nestjs/config';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { VisionService } from 'src/vision-service/vision-service';
+import { ImageOperationsService } from 'src/image-operations/image-operations.service';
+import { Vertex } from 'src/image-operations/image-operations.service';
+
+// ... [rest of the imports]
 
 @Controller('images')
 export class ImageStorageController {
@@ -19,6 +23,7 @@ export class ImageStorageController {
     private imageService: ImageStorageService,
     private config: ConfigService,
     private vision: VisionService,
+    private imageOps: ImageOperationsService,
   ) {}
 
   @Post('upload')
@@ -26,7 +31,8 @@ export class ImageStorageController {
   @Roles('ADMIN')
   @UseInterceptors(FilesInterceptor('images'))
   async uploadImage(@UploadedFiles() images: Express.Multer.File[]) {
-    const imageUrls = []; // Array to store public URLs
+    const imageUrls = [];
+    const croppedImageUrls = [];
 
     try {
       for (const image of images) {
@@ -38,14 +44,61 @@ export class ImageStorageController {
       }
 
       const visionResults = await this.vision.detectObjects({ imageUrls });
+      for (let i = 0; i < visionResults.length; i++) {
+        const analysis = visionResults[i];
+        const filenameFromUrl = new URL(imageUrls[i]).pathname.split('/').pop(); // This extracts the original file name
+        const imageBuffer = await this.imageService.getImageBuffer(
+          filenameFromUrl,
+          this.config.get('BUCKET_NAME'),
+        );
+
+        // To store the cropped URLs for this image.
+        const thisImageCroppedUrls = [];
+
+        analysis.forEach(async (detectedObject) => {
+          if (
+            detectedObject.boundingPoly &&
+            detectedObject.boundingPoly.normalizedVertices
+          ) {
+            const imageDimensions = await this.imageOps.getImageDimensions(
+              imageBuffer,
+            );
+            const pixelVertices: Vertex[] =
+              detectedObject.boundingPoly.normalizedVertices.map((v) => ({
+                x: v.x * imageDimensions.width,
+                y: v.y * imageDimensions.height,
+              }));
+
+            const croppedImageBuffer = await this.imageOps.cropImage(
+              imageBuffer,
+              pixelVertices,
+            );
+            const croppedUrl = await this.imageService.uploadCroppedImage(
+              croppedImageBuffer,
+              filenameFromUrl, // pass the original filename here
+              this.config.get('BUCKET_NAME'),
+            );
+
+            thisImageCroppedUrls.push(croppedUrl);
+          }
+        });
+
+        // If multiple cropped images were produced from one image, we store all their URLs.
+        if (thisImageCroppedUrls.length) {
+          croppedImageUrls.push(thisImageCroppedUrls);
+        }
+      }
+
       this.vision.saveResults({
         imageUrls: imageUrls,
         visionResults: visionResults,
       });
+
       return {
         status: 'success',
         data: {
           imageUrls: imageUrls,
+          croppedImageUrls: croppedImageUrls, // This will be an array of arrays now.
           analysis: visionResults,
         },
       };
--- src/image-storage/imageStorage.module.ts
+++ src/image-storage/imageStorage.module.ts
@@ -3,10 +3,11 @@ import { ImageStorageService } from './imageStorage.service';
 import { ImageStorageController } from './imageStorage.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { VisionService } from 'src/vision-service/vision-service';
+import { ImageOperationsService } from 'src/image-operations/image-operations.service';
 
 @Module({
   imports: [JwtModule],
-  providers: [ImageStorageService, VisionService],
+  providers: [ImageStorageService, VisionService, ImageOperationsService],
   controllers: [ImageStorageController],
 })
 export class ImageStorageModule {}
