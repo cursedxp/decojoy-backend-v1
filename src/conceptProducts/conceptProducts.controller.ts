8c0e642545faba6b32a6ac7d1c5707f5ed169297
Anil Ozsoy <anilozsoy@gmail.com>
2023-08-24 17:25:42 +0200

    🕹️updated controller method for add product to concept

--- src/conceptProducts/conceptProducts.controller.ts
+++ src/conceptProducts/conceptProducts.controller.ts
@@ -2,11 +2,14 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AddProductToConceptDto } from './dto';
 import { ConceptProductsService } from './conceptProducts.service';
 import { CreateAndAddProductToConceptDto } from './dto/createAndAddProduct.dto';
+import { ConceptProduct } from '@prisma/client';
 @Controller('conceptProducts')
 export class ConceptProductsController {
   constructor(private conceptProductService: ConceptProductsService) {}
   @Post('add-product-to-concept')
-  async addProductToConcept(@Body() dto: AddProductToConceptDto) {
+  async addProductToConcept(
+    @Body() dto: AddProductToConceptDto,
+  ): Promise<ConceptProduct> {
     return this.conceptProductService.addProductToConcept(dto);
   }
   @Post('create-and-add-product')
