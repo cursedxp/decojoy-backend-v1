1bb847135017b940f8595e42d8308d10be778ae0
Anil Ozsoy <anilozsoy@gmail.com>
2023-08-24 15:37:31 +0200

    🧹renamed module

---
+++ src/imageStorage/imageStorage.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ImageStorageService } from './imageStorage.service';
+import { ImageStorageController } from './imageStorage.controller';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [JwtModule],
+  providers: [ImageStorageService],
+  controllers: [ImageStorageController],
+})
+export class ImageStorageModule {}
