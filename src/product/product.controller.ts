33810cf4c9d5261c0ce961a97b6473b6af214d31
Anil Ozsoy <anilozsoy@gmail.com>
2023-08-24 15:42:31 +0200

    💂‍♀️guraded product routes

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 @Controller('products')
 export class ProductController {
   constructor(private productService: ProductService) {}
   @Post('create')
+  @UseGuards(AuthGuard)
+  @Roles('ADMIN')
   async create(@Body() createProductDto: CreateProductDto) {
     return this.productService.createProduct(createProductDto);
   }
   @Delete(':productId')
+  @UseGuards(AuthGuard)
+  @Roles('ADMIN')
   async delete(@Param('productId') productId: string) {
     return this.productService.deleteProduct(productId);
   }
   @Put(':conceptId')
+  @UseGuards(AuthGuard)
+  @Roles('ADMIN')
   async update(
     @Param('productId') productId: string,
     @Body() updateConceptDto: CreateProductDto,
