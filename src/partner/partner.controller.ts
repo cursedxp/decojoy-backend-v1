f347bc1749aba68833b65005972ae01bfc1ab8be
Anil Ozsoy <anilozsoy@gmail.com>
2023-08-24 22:18:22 +0200

    💂‍♀️guards has been added to project

--- src/partner/partner.controller.ts
+++ src/partner/partner.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { CreatePartnerDto } from './dto';
+import { PartnerService } from './partner.services';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('partner')
-export class PartnerController {}
+export class PartnerController {
+  constructor(private partnerService: PartnerService) {}
+  @Post('create-partner')
+  @UseGuards(AuthGuard)
+  @Roles('ADMIN')
+  async createPartner(@Body() data: CreatePartnerDto) {
+    return this.partnerService.createPartner(data);
+  }
+  @Post(':partnerId')
+  @UseGuards(AuthGuard)
+  @Roles('ADMIN')
+  async deletePartner(@Param() partnerId: string) {
+    return this.partnerService.deletePartner(partnerId);
+  }
+}
