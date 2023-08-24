bcd280231cbeceff643a639be55a0cf8553df14a
Anil Ozsoy <anilozsoy@gmail.com>
2023-08-24 17:24:43 +0200

    🔃 updated for partnerProducts

--- src/conceptProducts/dto/productToConcept.dto.ts
+++ src/conceptProducts/dto/productToConcept.dto.ts
@@ -1,4 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 export class AddProductToConceptDto {
+  @IsNotEmpty()
+  @IsString()
   conceptId: string;
+
+  @IsNotEmpty()
+  @IsString()
   productId: string;
+
+  @IsString()
+  partnerId?: string;
+  @IsString()
+  newPartnerName?: string;
 }
